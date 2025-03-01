3be15b77e5f2afec39821171c69139a4afa511f1
Laurel Thorburn <codinglaurel@gmail.com>
2025-03-01 12:00:28 -0700

    update meta data/favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Laurel King" },
+    { name: "description", content: "Learn more about Laurel King" },
   ];
 };
 
